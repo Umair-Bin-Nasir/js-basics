2906c3a966d20c68f406e215ca434cc972031b6e
Umair-Bin-Nasir <163725063+Umair-Bin-Nasir@users.noreply.github.com>
2024-04-04 19:33:46 -0700

    Add files via upload

---
+++ Assignment 03/index.ts
@@ -0,0 +1,41 @@
+// Question 01
+let movieName = "The Dark Knight";
+console.log("My favourite movie is " + movieName);
+// Question 02
+let myName = "Umair Bin Nasir";
+const bornIn = 2001;
+console.log("Hi, My name is " + myName + " and I was born in " + bornIn);
+// Question 03
+const mood = "angry";
+if(mood==="angry"){
+    console.log("Mood " + true);
+}
+// Question 04
+let firstName = "Umair";
+let lastName = "Nasir";
+console.log(firstName + " " + lastName);
+// Question 05
+let myAge = 23;
+console.log("My age is " + myAge);
+// Question 06
+let Product = "Smart Watch";
+let Price = 8000;
+console.log(`The Price of ${Product} is ${Price}`);
+// Question 07
+let Pets = 2;
+console.log(`Number of pets is  ${Pets}`);
+// Question 08
+let dateOfBirth = true;
+console.log("Today is your Birthday");
+// Question 09
+console.log(`My first name is ${firstName}, my last name is ${lastName} and my age is ${myAge}.`)
+// Question 10
+const birthYear = 2001;
+console.log(`Your Birth Year is ${birthYear}`);
+// Question 11
+let city = "Lahore";
+let country = "Pakistan";
+console.log(`Your address is Ittefaq colony kot lakhpat ${city}, ${country}`);
+// Question 12
+const temperature : number = 32;
+console.log(`Today's Temperature is ${temperature}`);
